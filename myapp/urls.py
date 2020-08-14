f3a96c5a4623f83976deedb6d8a493ad63acb589
Abhishek <abhiccc5@gmail.com>
2020-08-14 17:26:17 +0530

    added a view for / path

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,7 +5,7 @@ from . import views
 favicon_view = RedirectView.as_view(url='/static/images/favicon.ico', permanent=True)
 
 urlpatterns = [
-    path('/', views.groot, name='/')
+    path('/', views.groot, name='/'),
     path('', views.index, name='index'),
     path('getanswer', views.getanswer, name='getanswer'),
     path('favicon\.ico', favicon_view)
