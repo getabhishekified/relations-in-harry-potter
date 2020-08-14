7c617e8cb0b7e63a0feecd1ac0bfbd015400af4e
Abhishek <abhiccc5@gmail.com>
2020-08-14 17:01:12 +0530

    added favicon url in urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
+from django.views.generic.base import RedirectView
 from . import views
+
+favicon_view = RedirectView.as_view(url='/static/images/favicon.ico', permanent=True)
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('getanswer', views.getanswer, name='getanswer'),
+    re_path(r'^favicon\.ico$', favicon_view)
 ]
